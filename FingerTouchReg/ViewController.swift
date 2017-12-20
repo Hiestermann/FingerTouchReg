907c35cd5ce9fcef18ff53058812de007ac0a08e
Kilian Hiestermann <ian-h@gmx.net>
2017-12-20 14:14:22 +0100

    view full of color cubes

--- FingerTouchReg/ViewController.swift
+++ FingerTouchReg/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        for j in 0...30 {
+            for i in 0...numViewPerRow {
+                let squareView = UIView()
+                squareView.backgroundColor = randomColor()
+                squareView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width , width: width, height: width)
+                squareView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(squareView)
+            }
+        }
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+       let location = gesture.location(in: view)
+        print(location)
     }
-
-
+    
+    fileprivate func randomColor() -> UIColor{
+        
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 }
 
